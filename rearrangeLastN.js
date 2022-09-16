2dce68b310b4420b8cb793a0660e5905a3276b9c
bagl0025 <bagl0025@umn.edu>
2022-09-16 12:36:05 -0500

    minor update

---
+++ rearrangeLastN.js
@@ -0,0 +1,54 @@
+// function rearrangeLastN(l, n) {
+// }
+// User defined class node
+class ListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insertFirst(value) {
+    this.head = new ListNode(value, this.head);
+    this.size++;
+  }
+
+  printListData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+
+  insertLast(value) {
+    let node = new ListNode(value);
+    let current;
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+}
+
+var l = new LinkedList();
+l.insertFirst(5);
+l.insertFirst(4);
+l.insertFirst(3);
+l.insertFirst(2);
+l.insertFirst(1);
+l.printListData();
